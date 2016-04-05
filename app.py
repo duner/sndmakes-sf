fd9f1b573441712f7f3d5f3df5f3ef07c397cbbb
duner <asduner@gmail.com>
2016-04-05 10:41:59 -0700

    card structure

--- app.py
+++ app.py
@@ -19,6 +19,22 @@ def home():
     return render_template('home.html', **{'content': 'SNDMAKES!'})
 
 
+@app.route('/directory')
+def directory():
+    with open('static/new_sample.json') as datafile:
+        data = json.load(datafile)
+        people = [process_data_for_context(person) for person in data['people']]
+        return render_template('directory.html', **{
+            'people': people
+        })
+
+
+
+def process_data_for_context(person):
+    return person
+
+
+
 port = int(os.environ.get('PORT', 5000))
 if __name__ == '__main__':
     app.run(port=port, host='0.0.0.0')
